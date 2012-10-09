741fbc4c6e91428d3e8cc178ae4d5326f313565a
Darryl Ouye <Darryl.Ouye@sandisk.com>
2012-10-09 14:25:07 +0000

    #10038 TESTS_RAN: fdf_static_test Fixed compile issue
    git-svn-id: https://10.196.60.217/svn/schooner-products/branches/membrain-4.2/membrain/server@1312 01a69087-22d5-4a29-8152-8a1d5e10e5e9

--- api/tests/fdf_static_test.c
+++ api/tests/fdf_static_test.c
@@ -35,16 +35,13 @@ enum { NO_CPU_AFFINITY = -1 };
 volatile time_t 		 current_time;
 struct settings 		 settings;
 static struct SDF_state 	*sdf_state;
-//static struct SDF_thread_state 	*_sdf_thrd_state;
 
 
 // ====================================================================================================================
-static int container_test()
+static int container_test(struct SDF_thread_state *_sdf_thread_state)
 {
     char				*cname = "/cache_container";
     SDF_container_props_t		 p;
-    SDF_thread_state_t			*sdf_thread_state;
-    uint64_t 				 cid = 1;
     SDF_cguid_t				 cguid = SDF_NULL_CGUID;
     char				*key = "object1";
     int					 keylen = strlen(key);
@@ -52,61 +49,69 @@ static int container_test()
     int					 datalen = strlen(data);
     char				*buf = NULL;
     uint64_t 				 buflen = 0;
+    SDF_time_t    			 expiry;
 
-    p.container_type.type = SDF_OBJECT_CONTAINER;
-    p.container_id.size = 1000; /* KB */
-    cguid = SDFGenerateCguid(sdf_thread_state, cid);
+    p.container_id.size 		= 1024; /* KB */
+    p.durability_level 			= SDF_FULL_DURABILITY;
+    p.fifo_mode 			= SDF_FALSE; 
+    p.container_type.type 		= SDF_OBJECT_CONTAINER;
+    p.container_type.caching_container 	= SDF_TRUE;
+    p.container_type.persistence 	= SDF_TRUE;
+    p.container_type.async_writes 	= SDF_FALSE;
+    p.cache.writethru 			= SDF_TRUE;
+    p.container_id.num_objs 		= 10000; 
+    p.shard.num_shards 			= 1;
 
-    if (SDF_SUCCESS == SDFCreateContainer(sdf_thread_state, cname, &p, &cguid)) {
+    if (SDF_SUCCESS == SDFCreateContainer(_sdf_thread_state, cname, &p, &cguid)) {
 	fprintf(stderr, "Created container %s\n", cname);
     } else {
 	fprintf(stderr, "Error creating container %s\n", cname);
 	return (-1);
     }
 
-    if (SDF_SUCCESS == (SDFOpenContainer(sdf_thread_state, cguid, SDF_READ_WRITE_MODE))) {
+    if (SDF_SUCCESS == (SDFOpenContainer(_sdf_thread_state, cguid, SDF_READ_WRITE_MODE))) {
 		fprintf(stderr, "Opened container %s.\n", cname);
 	} else {
 		fprintf(stderr, "Error opening container %s.\n", cname);
         return (-1);
     }
 
-    if (SDF_SUCCESS == (SDFStartContainer(sdf_thread_state, cguid))) {
+    if (SDF_SUCCESS == (SDFStartContainer(_sdf_thread_state, cguid))) {
 	fprintf(stderr, "Started container %s.\n", cname);
     } else {
 	fprintf(stderr, "Error starting container %s.\n", cname);
         return (-1);
     }
 
-    if (SDF_SUCCESS == (SDFPutBufferedObject(sdf_thread_state, cguid, key, keylen, data, datalen, 0, 0))) {
-	fprintf(stderr, "Put object %s.\n", key);
+    if (SDF_SUCCESS == (SDFCreateBufferedObject(_sdf_thread_state, cguid, key, keylen, data, datalen, 0, 0))) {
+	fprintf(stderr, "Create object %s.\n", key);
     } else {
 	fprintf(stderr, "Error putting object %s.\n", key);
         return (-1);
     }
 
-    if (SDF_SUCCESS == (SDFGetForReadBufferedObject(sdf_thread_state, cguid, key, keylen, &buf, &buflen, 0, 0))) {
+    if (SDF_SUCCESS == (SDFGetForReadBufferedObject(_sdf_thread_state, cguid, key, keylen, &buf, &buflen, 0, &expiry))) {
 	fprintf(stderr, "Get object %s, value = %s\n", key, buf);
     } else {
 	fprintf(stderr, "Error getting object %s.\n", key);
         return (-1);
     }
 
-    if (SDF_SUCCESS == (SDFStopContainer(sdf_thread_state, cguid))) {
+    if (SDF_SUCCESS == (SDFStopContainer(_sdf_thread_state, cguid))) {
 	fprintf(stderr, "Stopped container %s.\n", cname);
     } else {
 	fprintf(stderr, "Error stoping container %s.\n", cname);
         return (-1);
     }
 
-    if (SDF_SUCCESS == (SDFCloseContainer(sdf_thread_state, cguid))) {
+    if (SDF_SUCCESS == (SDFCloseContainer(_sdf_thread_state, cguid))) {
 	fprintf(stderr, "Closed container %s.\n", cname);
     } else {
 	fprintf(stderr, "Error closing container %s.\n", cname);
         return (-1);
     }
 
-    if (SDF_SUCCESS == SDFDeleteContainer(sdf_thread_state, cguid)) {
+    if (SDF_SUCCESS == SDFDeleteContainer(_sdf_thread_state, cguid)) {
 	fprintf(stderr, "Deleted container %s\n", cname);
     } else {
 	fprintf(stderr, "Error Deleting container %s\n", cname);
@@ -120,25 +125,17 @@ static int container_test()
 int
 main(int argc, char *argv[])
 {
-#if 0
-    int affinity = NO_CPU_AFFINITY;
-    char *cname = "foo";
-    api_settings_init();
+    struct SDF_thread_state *_sdf_thread_state;
 
-    api_init_sdf( settings.sdf_log_level,
-                  settings.num_sdf_threads,  // # of fthreads
-                  settings.num_cores,        // # of cores
-                  affinity,                  // CPU affinity or -1
-                  cname,
-                  argc,     
-                  argv    );
-#endif
-
-    if (SDF_SUCCESS == SDFInit(&sdf_state, argc, argv)) {
-	return (container_test());
-    } else {
-	fprintf(stderr, "Failed to initialize SDF\n");
-	return (-1);
+    if (SDFInit(&sdf_state, 0, NULL) != SDF_SUCCESS) {
+        fprintf(stderr, "SDF initialization failed!\n");
+        plat_assert(0);
     }
+
+    fprintf(stderr, "SDF was initialized successfully!\n");
+
+    _sdf_thread_state    = SDFInitPerThreadState(sdf_state);
+
+    container_test(_sdf_thread_state);
 }
 
