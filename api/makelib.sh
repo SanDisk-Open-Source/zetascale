05f2255ca4a5c1201a97237741395cb8e8fa77c8
Evgeniy Firsov <Evgeniy.Firsov@sandisk.com>
2012-11-09 00:38:56 +0000

    #00000
    TESTS_RAN: none
    
    optimized version of helper makelib.sh
    
    
    
    git-svn-id: https://10.196.60.217/svn/schooner-products/branches/membrain-4.2/membrain/server@1369 01a69087-22d5-4a29-8152-8a1d5e10e5e9

--- api/makelib.sh
+++ api/makelib.sh
@@ -1,31 +1,31 @@
 #!/bin/bash -x
 
 echo >tmp
-for i in  ../../build/sdf/api/libsdf.a \
-	../../build/sdf/agent/libsdfagent.a \
-	../../build/sdf/shared/libsdfshared.a \
-    ../../build/sdf/protocol/action/libsdfaction.a \
-    ../../build/sdf/protocol/home/libsdfhome.a \
-	../../build/sdf/protocol/libsdfprotocol.a \
-	../../build/sdf/ssd/libsdfssd.a \
-	../../build/sdf/ssd/clipper/libclipper.a \
-	../../build/sdf/ssd/fifo/libfifo.a \
-	../../build/sdf/ssd/libsdfssd.a \
-	../../build/sdf/protocol/replication/libsdfreplication.a \
-	../../build/sdf/ecc/libecc.a \
-	../../build/sdf/utils/libutils.a \
-	../../build/sdf/sdfmsg/libsdfmsgqueue.a \
-	../../build/sdf/sdftcp/libsdfmtp.a \
-	../../build/sdf/platform/tool/hotkey/libhotkey.a \
-	../../build/sdf/misc/libmisc.a \
-	../../build/sdf/fth/libfth.a \
-	../../build/sdf/platform/libplatform.a
-# ../../build/../3rd-party/libevent-1.4.3-et/lib/libevent.a
+for i in  ../../build/sdf/api/libsdf_o.a \
+	../../build/sdf/agent/libsdfagent_o.a \
+	../../build/sdf/shared/libsdfshared_o.a \
+    ../../build/sdf/protocol/action/libsdfaction_o.a \
+    ../../build/sdf/protocol/home/libsdfhome_o.a \
+	../../build/sdf/protocol/libsdfprotocol_o.a \
+	../../build/sdf/ssd/libsdfssd_o.a \
+	../../build/sdf/ssd/clipper/libclipper_o.a \
+	../../build/sdf/ssd/fifo/libfifo_o.a \
+	../../build/sdf/ssd/libsdfssd_o.a \
+	../../build/sdf/protocol/replication/libsdfreplication_o.a \
+	../../build/sdf/ecc/libecc_o.a \
+	../../build/sdf/utils/libutils_o.a \
+	../../build/sdf/sdfmsg/libsdfmsgqueue_o.a \
+	../../build/sdf/sdftcp/libsdfmtp_o.a \
+	../../build/sdf/platform/tool/hotkey/libhotkey_o.a \
+	../../build/sdf/misc/libmisc_o.a \
+	../../build/sdf/fth/libfth_o.a \
+	../../build/sdf/platform/libplatform_o.a
+# ../../build/../3rd-party/libevent-1.4.3-et/lib/libevent_o.a
 do
 ar t $i|sed "s+^+$(dirname $i)/+" >>tmp
 done
 
-ar -sr libsdf.a $(cat tmp) ../../build/../3rd-party/libevent-1.4.3-et/lib/libevent.so
+ar -sr libsdf.a $(cat tmp) ../../../3rd-party/libevent-1.4.3-et/lib/libevent.so
 
 #ranlib libsdf.o
-# -lpthread -lrt -lnsl -lutil -lm -la.a -lsasl2
+# -lpthread -lrt -lnsl -lutil -lm -la_o.a -lsasl2
