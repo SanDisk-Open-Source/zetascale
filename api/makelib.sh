8811906e689707fc52cbbd76d63feced86ed1ff3
Manavalan Krishnan <Manavalan.Krishnan@sandisk.com>
2012-11-28 00:02:04 +0000

    #0000 TESTS_RAN:None Enhanced script to quickly build libfdf.a
    git-svn-id: https://10.196.60.217/svn/schooner-products/branches/membrain-4.2/membrain/server@1440 01a69087-22d5-4a29-8152-8a1d5e10e5e9

--- api/makelib.sh
+++ api/makelib.sh
@@ -1,31 +1,50 @@
 #!/bin/bash -x
 
+OPTIMIZE=""
+if [ "$1" = "help" ]
+then
+    echo
+    echo "$0 [optimize|help]"
+    echo optimize - build library with all optimization
+    echo help - display help message
+    echo
+    exit 0    
+fi
+
+if [ "$1" = "optmize" ]
+then
+    OPTIMIZE="_o" 
+    make optimize
+else
+    make
+fi
+
 echo >tmp
-for i in  ../../build/sdf/api/libsdf_o.a \
-	../../build/sdf/agent/libsdfagent_o.a \
-	../../build/sdf/shared/libsdfshared_o.a \
-    ../../build/sdf/protocol/action/libsdfaction_o.a \
-    ../../build/sdf/protocol/home/libsdfhome_o.a \
-	../../build/sdf/protocol/libsdfprotocol_o.a \
-	../../build/sdf/ssd/libsdfssd_o.a \
-	../../build/sdf/ssd/clipper/libclipper_o.a \
-	../../build/sdf/ssd/fifo/libfifo_o.a \
-	../../build/sdf/ssd/libsdfssd_o.a \
-	../../build/sdf/protocol/replication/libsdfreplication_o.a \
-	../../build/sdf/ecc/libecc_o.a \
-	../../build/sdf/utils/libutils_o.a \
-	../../build/sdf/sdfmsg/libsdfmsgqueue_o.a \
-	../../build/sdf/sdftcp/libsdfmtp_o.a \
-	../../build/sdf/platform/tool/hotkey/libhotkey_o.a \
-	../../build/sdf/misc/libmisc_o.a \
-	../../build/sdf/fth/libfth_o.a \
-	../../build/sdf/platform/libplatform_o.a
-# ../../build/../3rd-party/libevent-1.4.3-et/lib/libevent_o.a
+for i in  ../../build/sdf/api/libsdf$OPTIMIZE.a \
+	../../build/sdf/agent/libsdfagent$OPTIMIZE.a \
+	../../build/sdf/shared/libsdfshared$OPTIMIZE.a \
+    ../../build/sdf/protocol/action/libsdfaction$OPTIMIZE.a \
+    ../../build/sdf/protocol/home/libsdfhome$OPTIMIZE.a \
+	../../build/sdf/protocol/libsdfprotocol$OPTIMIZE.a \
+	../../build/sdf/ssd/libsdfssd$OPTIMIZE.a \
+	../../build/sdf/ssd/clipper/libclipper$OPTIMIZE.a \
+	../../build/sdf/ssd/fifo/libfifo$OPTIMIZE.a \
+	../../build/sdf/ssd/libsdfssd$OPTIMIZE.a \
+	../../build/sdf/protocol/replication/libsdfreplication$OPTIMIZE.a \
+	../../build/sdf/ecc/libecc$OPTIMIZE.a \
+	../../build/sdf/utils/libutils$OPTIMIZE.a \
+	../../build/sdf/sdfmsg/libsdfmsgqueue$OPTIMIZE.a \
+	../../build/sdf/sdftcp/libsdfmtp$OPTIMIZE.a \
+	../../build/sdf/platform/tool/hotkey/libhotkey$OPTIMIZE.a \
+	../../build/sdf/misc/libmisc$OPTIMIZE.a \
+	../../build/sdf/fth/libfth$OPTIMIZE.a \
+	../../build/sdf/platform/libplatform$OPTIMIZE.a
+# ../../build/../3rd-party/libevent-1.4.3-et/lib/libevent$OPTIMIZE.a
 do
 ar t $i|sed "s+^+$(dirname $i)/+" >>tmp
 done
 
-ar -sr libsdf.a $(cat tmp) ../../../3rd-party/libevent-1.4.3-et/lib/libevent.so
+ar -sr libfdf.a $(cat tmp) ../../../3rd-party/libevent-1.4.3-et/lib/libevent.so
 
 #ranlib libsdf.o
-# -lpthread -lrt -lnsl -lutil -lm -la_o.a -lsasl2
+# -lpthread -lrt -lnsl -lutil -lm -la$OPTIMIZE.a -lsasl2
