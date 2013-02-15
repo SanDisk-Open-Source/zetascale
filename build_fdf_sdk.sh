c610af13d9c8466b9eb0b057e8c541da495950ed
Evgeniy Firsov <Evgeniy.Firsov@sandisk.com>
2013-02-15 23:55:51 +0000

    #00000 Make debug build default(--optimize for release build), get rid of libfdfdbg.so TESTS_RAN: none
    git-svn-id: https://10.196.60.217/svn/schooner-products/branches/membrain-4.3/membrain/server@1540 01a69087-22d5-4a29-8152-8a1d5e10e5e9

--- build_fdf_sdk.sh
+++ build_fdf_sdk.sh
@@ -30,37 +30,32 @@ mkdir -p ${membrain_sdk_dir}/{config,lib,include,samples,tests,docs}
 
 cd ${mWORKSPACE}
 
-#debug shared lib
-if [ "$1" == "--debug" ] || [ "$2" == "--debug" ]; then
-echo "Building DEBUG shared lib"
-rm -f CMakeCache.txt
-cmake ${mWORKSPACE} -DDEBUG=ON -DFDF_REVISION="${FDF_SDK_VERSION}-${SCHOONER_RELEASE}"
-make -j $CPU
-cp -fv ${mWORKSPACE}/output/lib/* ${membrain_sdk_dir}/lib
-make clean
-fi
-#
-#optimized shared lib
-echo "Building OPTIMIZED shared lib"
-rm -f CMakeCache.txt
-cmake ${mWORKSPACE} -DFDF_REVISION="${FDF_SDK_VERSION}-${SCHOONER_RELEASE}" #Default: -DDEBUG=OFF -DBUILD_SHARED=ON
 CPU=$(cat /proc/cpuinfo|grep CPU|wc -l)
+
+rm -f CMakeCache.txt
+
+DBG=ON
+if [ "$1" == "--optimize" ] || [ "$2" == "--optimize" ]; then
+	DBG=OFF
+fi
+
+echo "Building DEBUG=$DBG shared lib"
+cmake ${mWORKSPACE} -DDEBUG=$DBG -DFDF_REVISION="${FDF_SDK_VERSION}-${SCHOONER_RELEASE}"
 make -j $CPU
-#ctest
 cp -fv ${mWORKSPACE}/output/lib/* ${membrain_sdk_dir}/lib
-#
+
 #
 cp -av ${mWORKSPACE}/api/fdf.h ${membrain_sdk_dir}/include
 mkdir -p ${membrain_sdk_dir}/include/common
 cp -va ${mWORKSPACE}/common/fdf{stats,types}.h ${membrain_sdk_dir}/include/common
 #
 cd ${membrain_sdk_dir}/..
-tar --exclude=.svn --exclude=.git --exclude=libfdf.a --exclude=libfdfdbg.a -czf ${mWORKSPACE}/fdf_sdk-${FDF_SDK_VERSION}-${SCHOONER_RELEASE}.tar.gz fdf_sdk-${FDF_SDK_VERSION}-${SCHOONER_RELEASE}
+tar --exclude=.svn --exclude=.git --exclude=libfdf.a -czf ${mWORKSPACE}/fdf_sdk-${FDF_SDK_VERSION}-${SCHOONER_RELEASE}.tar.gz fdf_sdk-${FDF_SDK_VERSION}-${SCHOONER_RELEASE}
 rm -fr fdf_sdk-${FDF_SDK_VERSION}-${SCHOONER_RELEASE}
 
 echo -e "\n** BUILD SUCCESSFUL **\n"
 
-echo -e "\n** Please set the following variables and modify FDF_PROPERTY_FILE\nto conform your envvironment in order to run 'make test':\nexport FDF_LIB=${mWORKSPACE}/output/lib/libfdf.so\nexport FDF_PROPERTY_FILE=${mWORKSPACE}/api/tests/conf/fdf.prop\n**\n"
+echo -e "\n** Please set the following variables and modify FDF_PROPERTY_FILE\nto conform your environment in order to run 'make test':\nexport FDF_LIB=${mWORKSPACE}/output/lib/libfdf.so\nexport FDF_PROPERTY_FILE=${mWORKSPACE}/api/tests/conf/fdf.prop\n**\n"
 
 #Running tests
 if [ "$1" == "--test" ] || [ "$2" == "--test" ]; then
