4cb09125033c0e575cb1f85203a74f182d475641
Harihara Kadayam <Harihara.Kadayam@sandisk.com>
2013-05-24 00:51:47 +0000

    #00000 Small fix to avoid core dump while displaying help for btree life cycle test TESTS_RAN: Manually
    git-svn-id: https://10.196.60.217/svn/schooner-products/branches/fdf-2.0@1791 01a69087-22d5-4a29-8152-8a1d5e10e5e9

--- btree/tests/btest_life_cycle.c
+++ btree/tests/btest_life_cycle.c
@@ -17,6 +17,9 @@ int main(int argc, char *argv[])
 	btest_cfg_t *cfg;
 
 	cfg = btest_init(argc, argv, "btest_lifecycle_test", NULL);
+	if (cfg == NULL) {
+		return 0;
+	}
 	
 	btest_rand_data_gen(cfg);
 	btest_life_cycle(cfg);
