SanDisk-Open-Source/zetascale

commit e402398023182416666be94b53db642ffc5d4809

#00000 Two malloc bugs in crash recovery Fixed a bug in trx "undo" where a counter was mistakenly incremented, causing malloc buffer overrun and segfault.  Fixed another malloc overrun bug regarding slab decompression: compressed size can be larger than decompressed!  Crash recovery made 5x faster (new data format, gzip options). TESTS_RAN: Unit tests, DevTest #596, fdfslap #451, custom tests
git-svn-id: https://10.196.60.217/svn/schooner-products/branches/fdf-2.0@2106 01a69087-22d5-4a29-8152-8a1d5e10e5e9

btree/packet.c
@@ -250,14 +250,16 @@ readlines( rec_packet_t *r, uint *count, int old)
 		while (*p)
 			if (*p == '\\') {
 				unless ((p[1])
-				and (p[2])
-				and (p[3])) {
+				and (p[2])) {
 					baddata( );
 					return (0);
 				}
-				*q++ = p[1]-'0'<<2*3 | p[2]-'0'<<1*3 | p[3]-'0'<<0*3;
-				p += 4;
+				static char HEX[] = "0123456789ABCDEF";
+				*q++ = strchr( HEX, p[1])-HEX<<4 | strchr( HEX, p[2])-HEX;
+				p += 3;
 			}
+			else if (*p == '@')
+				*q++ = 0;
 			else
 				*q++ = *p++;
 		unless (c < nel( r->lvec)) {
@@ -306,12 +308,16 @@ statsreadline( stats_packet_t *s)
 	while (*p)
 		if (*p == '\\') {
 			unless ((p[1])
-			and (p[2])
-			and (p[3]))
-				return (badstatsdata( ));
-			*q++ = p[1]-'0'<<2*3 | p[2]-'0'<<1*3 | p[3]-'0'<<0*3;
-			p += 4;
+			and (p[2])) {
+				baddata( );
+				return (0);
+			}
+			static char HEX[] = "0123456789ABCDEF";
+			*q++ = strchr( HEX, p[1])-HEX<<4 | strchr( HEX, p[2])-HEX;
+			p += 3;
 		}
+		else if (*p == '@')
+			*q++ = 0;
 		else
 			*q++ = *p++;
 	s->nbyte = q - s->lbuf1;

btree/btree_recovery.c
@@ -113,7 +113,6 @@ gen_robj_list(btree_raw_t *bt, btree_raw_node_t **nodes,
 	uint32_t alloc_cnt = 0;
 	uint32_t used_cnt = 0;
 	btree_robj_info_t *robj_buf = NULL;
-	key_stuff_t ks;
 	btree_robj_info_t *r;
 	btree_robj_info_t *e;
 	btree_robj_info_t *prev_e;
@@ -145,8 +144,6 @@ gen_robj_list(btree_raw_t *bt, btree_raw_node_t **nodes,
 		}
 
 		for (j = nodes[i]->nkeys - 1; j >= 0; j--) {
-			(void) get_key_stuff(bt, nodes[i], j, &ks);
-
 			if (used_cnt == alloc_cnt) {
 				btree_robj_info_t *tmp_buf = robj_buf;
 
@@ -157,7 +154,7 @@ gen_robj_list(btree_raw_t *bt, btree_raw_node_t **nodes,
 				robj_buf = malloc(node_cnt * AVG_KEY_COUNT 
 				                  * sizeof(btree_robj_info_t));
 				assert(robj_buf);
-				alloc_cnt += node_cnt * AVG_KEY_COUNT;
+				//alloc_cnt += node_cnt * AVG_KEY_COUNT;
 				tmp_buf->buf_next = robj_buf;
 
 				used_cnt = 0;
@@ -338,7 +335,9 @@ do_recovery_op(btree_raw_t *bt, btree_robj_list_t *op_list)
 				exit(0);
 			}
 
+#if 0//Rico - buggy?
 			free(cur_obj->key);
+#endif
 			break;
 
 		case RCVR_OP_SET:
@@ -362,8 +361,10 @@ do_recovery_op(btree_raw_t *bt, btree_robj_list_t *op_list)
 			meta.seqno = cur_obj->seqno;
 			status = btree_raw_write(bt, cur_obj->key, cur_obj->keylen, 
 			                 cur_obj->data, cur_obj->datalen, &meta, 0);
+#if 0//Rico - buggy?
 			free(cur_obj->key);
 			free(cur_obj->data);
+#endif
 
 			if (status == BTREE_SUCCESS) {
 				stats_set_cnt++;

ssd/fifo/mcd_rec.c
@@ -5922,7 +5922,7 @@ recovery_packet_dump( FILE *f, uint32_t blkno, uint32_t nblock, size_t ocount, v
 	char *data = buf + sizeof( mcd_osd_meta_t) + meta->key_len;
 	char *data2 = 0;
 	if (meta->uncomp_datalen) {
-		data2 = malloc( meta->uncomp_datalen);
+		data2 = malloc( max( dlen, meta->uncomp_datalen));
 		memcpy( data2, data, dlen);
 		data = data2;
 		size_t udlen;
@@ -5938,11 +5938,17 @@ recovery_packet_dump( FILE *f, uint32_t blkno, uint32_t nblock, size_t ocount, v
 		ocount = dlen;
 	for (uint i=0; i<ocount; ++i) {
 		uint c = ((uchar *)data)[i];
-		if ((' '<c && c<0177)
-		and (c != '\\'))
+		unless (c)
+			putc_unlocked( '@', f);
+		else if ((' '<c && c<0177)
+		and (c != '\\')
+		and (c != '@'))
 			putc_unlocked( c, f);
-		else
-			fprintf( f, "\\%03o", c);
+		else {
+			putc_unlocked( '\\', f);
+			putc_unlocked( "0123456789ABCDEF"[(c>>1*4)%16], f);
+			putc_unlocked( "0123456789ABCDEF"[(c>>0*4)%16], f);
+		}
 	}
 	fputc( '\n', f);
 	if (data2)
@@ -5986,7 +5992,7 @@ recovery_packet_save( packet_t *r, void *context, mcd_osd_shard_t *shard)
 		j;
 
 	char *SAVE_COMMAND = "sort -k 1,1 -k 3,3nr -k 5,5 -k 4,4n |"
-		"awk '{print | \"gzip >\"ENVIRON[\"FDFRECOVERYDIR\"]\"/cguid-\"$1\".gz\"}'";
+		"awk '{print | \"gzip -1 >\"ENVIRON[\"FDFRECOVERYDIR\"]\"/cguid-\"$1\".gz\"}'";
 	fflush( stdout);
 	for (i=0; i<nel( r->btab); ++i)
 		while ((j = r->btab[i].nrec)

commit 17cae894a94756d80a877ea888949d8cab22f7a8

#11363 FDF won't rollback after recovery Fixed stupid typo introduced by r2106 that caused packet truncation. TESTS_RAN: Unit tests, t/repro_bugs/11363.t, DevTest #607, custom tests
git-svn-id: https://10.196.60.217/svn/schooner-products/branches/fdf-2.0@2114 01a69087-22d5-4a29-8152-8a1d5e10e5e9

btree/packet.c
@@ -259,7 +259,7 @@ readlines( rec_packet_t *r, uint *count, int old)
 				p += 3;
 			}
 			else if (*p == '@')
-				*q++ = 0;
+				*q++ = 0, p++;
 			else
 				*q++ = *p++;
 		unless (c < nel( r->lvec)) {
@@ -317,7 +317,7 @@ statsreadline( stats_packet_t *s)
 			p += 3;
 		}
 		else if (*p == '@')
-			*q++ = 0;
+			*q++ = 0, p++;
 		else
 			*q++ = *p++;
 	s->nbyte = q - s->lbuf1;