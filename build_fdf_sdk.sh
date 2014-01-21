993afd940b33a7d397e57f9a32f5e2f6939c69be
Yi Zhang <Yi.Zhang@sandisk.com>
2014-01-21 09:34:27 +0000

    #9999 TESTS_RAN: Manual add "--with-jni" option to build scripts with this option libfdfjni.so will be build and packaged in fdf_sdk package
    git-svn-id: https://10.196.60.217/svn/schooner-products/branches/fdf-2.0@2131 01a69087-22d5-4a29-8152-8a1d5e10e5e9

--- build_fdf_sdk.sh
+++ build_fdf_sdk.sh
@@ -8,6 +8,7 @@ while [ $# -gt 0 ]; do
 	[ "$1" == "--test" ] && run_tests=$1
 	[ "$1" == "--pkg" ] && pkg=$1
 	[ "$1" == "--trace" ] && TRACE=ON
+	[ "$1" == "--with-jni" ] && WITHJNI=ON
 	shift
 done
 
@@ -66,6 +67,23 @@ mkdir -p $SDK_DIR/include/common
 cp -a $WD/common/fdftypes.h $SDK_DIR/include/common
 cp -a $WD/api/tests/conf/fdf_sample.prop $SDK_DIR/config/
 #
+#when withjni=true, get jni code and compiling
+if [ "is$WITHJNI" == "isON" ]
+then
+    rm -fr fdfjni_2.0
+    jniurl=http://svn.schoonerinfotech.net/svn/schooner-trunk/ht_delivery/rd/fdfjni/branches/fdfcassandra_2.0
+    svn co $jniurl fdfjni_2.0
+    cd fdfjni_2.0
+    cp -r $SDK_DIR ./fdf_sdk
+	export BTREE_LIB=$PWD/fdf_sdk/lib/libbtree.so
+	export FDF_LIB=$PWD/fdf_sdk/lib/libfdf.so
+    cd src/main/c/Release/ && make clean && make && cp libfdfjni.so $SDK_DIR/lib/
+    cd - 
+    cd ..
+fi
+#
+
+
 cd $SDK_DIR/..
 tar --exclude=.svn --exclude=.git --exclude=libfdf.a -czf $WD/$PKG_NAME.tar.gz $PKG_NAME
 if [ -n "$pkg" ]; then
