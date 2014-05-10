1ce99131ebc4f7e48e0838a2326197496f7e33ca
Vishal Kanaujia <Vishal.Kanaujia@sandisk.com>
2014-05-10 05:43:57 +0000

    #11431 TESTS_RAN: Asynchronous stats recovery stats tests and dev test The checkin resolves few issues in async stats recovery Turns on Async stats recovery by default Adds some configuration variable
    git-svn-id: https://10.196.60.217/svn/schooner-products/branches/fdf-2.1@2293 01a69087-22d5-4a29-8152-8a1d5e10e5e9

--- api/tests/FDF_Pstats.c
+++ api/tests/FDF_Pstats.c
@@ -1,16 +1,28 @@
 #include "fdf.h"
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include <unistd.h>
-#define FDF_MAX_KEY_LEN 256
 
+#define FDF_MAX_KEY_LEN 256
+#define NWORKER 4
 #define OBJ_COUNT 100000
 
-int
-main()
+static int count = 0;
+
+typedef struct worker_arg_ {
+    int id;
+} worker_arg_t;
+
+struct FDF_state *fdf_state;
+
+void worker(void *ptr);
+
+
+void worker(void *ptr)
 {
+    assert(ptr);
     FDF_container_props_t props;
-    struct FDF_state *fdf_state;
     struct FDF_thread_state *thd_state;
     struct FDF_iterator *iterator;
     FDF_status_t	status;
@@ -20,15 +32,10 @@ main()
     char	*data = NULL;
     char	data1[256] = "data is just for testing pstats";
     int i;
-    int count = 0;
     uint32_t keylen;
-
+    char cname[FDF_MAX_KEY_LEN];
     char key_var[FDF_MAX_KEY_LEN] ={0};
 
-    FDFSetProperty("FDF_REFORMAT", "1");
-    if (FDFInit(&fdf_state) != FDF_SUCCESS) {
-		return -1;
-	}
     FDFInitPerThreadState(fdf_state, &thd_state);	
 
     FDFLoadCntrPropDefaults(&props);
@@ -41,43 +48,89 @@ main()
     //	props.fifo_mode = 1;
     props.fifo_mode = 0;
 
-    status = FDFOpenContainer(thd_state, "cntr", &props, FDF_CTNR_CREATE, &cguid);
+    //int id = ((worker_arg_t*)ptr)->id;
+    sprintf(cname,  "cntr_%d", __sync_fetch_and_add(&(count), 1));
+    printf("worker:cname=%s\n", cname);
+
+    status = FDFOpenContainer(thd_state, cname, &props, FDF_CTNR_CREATE, &cguid);
     if (status != FDF_SUCCESS) {
         printf("Open Cont failed with error=%x.\n", status);
-        return -1;	
+        exit(0);//return -1;	
     }
 
+    status = FDFOpenContainer(thd_state, cname, &props, FDF_CTNR_RW_MODE, &cguid);
+    if (status != FDF_SUCCESS) {
+        printf("Open Cont failed with error=%x.\n", status);
+        exit(0);//return -1;	
+    }
     for (i = 1; i <= OBJ_COUNT; i++) {
         memset(key_var, 0, FDF_MAX_KEY_LEN);
-        sprintf(key_var, "key_%d", i);
-        sprintf(data1, "data_%d", i);
+        sprintf(key_var, "key_%08d", i);
+        sprintf(data1, "data_%08d", i);
 
         FDFWriteObject(thd_state, cguid, key_var, strlen(key_var) + 1, data1, strlen(data1), 0);
     }
 
-    count = 0;
+    for (i = 1; i <= OBJ_COUNT/1000; i++) {
+        memset(key_var, 0, FDF_MAX_KEY_LEN);
+        sprintf(key_var, "key_%08d", i);
+        status = FDFDeleteObject(thd_state, cguid, key_var, strlen(key_var) + 1);
+        if (status != FDF_SUCCESS)
+            fprintf(stderr, "FDFDELETE RETURNED %s\n", FDFStrError(status));
+    }
+
+# if 0
+    //  _FDFScavengeContainer(struct FDF_state *fdf_state, FDF_cguid_t cguid, bool is_sync)
+    status = FDFScavengeContainer(fdf_state, cguid);
+    fprintf(stderr, "FDFScavengeContainer returns %s\n", FDFStrError(status));
+#endif
     status =  FDFEnumerateContainerObjects(thd_state, cguid, &iterator);
 
+    int ii = 0;
     while (FDFNextEnumeratedObject(thd_state, iterator, &key, &keylen, &data, &datalen) == FDF_SUCCESS) {
+        if (0 == (ii % 12345)) {
+            fprintf(stderr, "key = %s keylen=%d\n", key, keylen);
+        }
+        ii++;
         FDFFreeBuffer(data);
         FDFFreeBuffer(key);
-        count++;
     }
     FDFFinishEnumeration(thd_state, iterator);
 
-    FDF_stats_t stats;
-    status = FDFGetContainerStats(thd_state, cguid, &stats);
-    printf("Enum count=%d Pstats count=%ld size = %ld\n",
-            count, stats.cntr_stats[FDF_CNTR_STATS_NUM_OBJS], stats.cntr_stats[FDF_CNTR_STATS_USED_SPACE]);
-
     FDFCloseContainer(thd_state, cguid);
     FDFReleasePerThreadState(&thd_state);
+}
 
-    FDFShutdown(fdf_state);
 
-    if (stats.cntr_stats[FDF_CNTR_STATS_NUM_OBJS] == count) {
-        return 0;
-    } else {
-    return 1;
+int
+main()
+{
+    FDF_status_t status;
+    FDFSetProperty("FDF_REFORMAT", "1");
+    status = FDFInit(&fdf_state);
+
+    if (FDF_SUCCESS != status) {
+      fprintf(stderr, "FDFInit: error=%s\n", FDFStrError(status));
+      exit(0);
     }
+
+    pthread_t thread[32];
+    int  iret[32];
+    /* Create independent threads each of which will execute function */
+
+    int i;
+    for (i = 0; i < NWORKER; i++) {
+        worker_arg_t arg;
+        arg.id = 0;
+        //(void) __sync_fetch_and_add(&(arg.id), 1);
+        arg.id += i;
+        iret[i] = pthread_create( &thread[i], NULL, (void *)worker, (void*)&arg);
+        fprintf(stderr,"Create worker Thread %d\n",arg.id);
+    }
+
+    for (i = 0; i < NWORKER; i++) {         
+        pthread_join( thread[i], NULL);
+    }
+    FDFShutdown(fdf_state);
+    exit(0);
 }
