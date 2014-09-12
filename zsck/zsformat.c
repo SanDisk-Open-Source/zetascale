8fa0b17885b8f34223621a459e3b2c3ef90fd1d7
Manavalan Krishnan <Manavalan.Krishnan@sandisk.com>
2014-09-12 00:05:52 +0000

    #FDF-107 TESTS_RAN:Given below
    This checkin adds an utlity called zsformat. Application can use this utlity to format the zetascale storage before bringing up ZetaScale first time.
    This utlity has teh following syntax:
    ./zsck/zsformat --help
    Formats the ZetaScale Storage
    Set the enviornmental variable: ZS_PROPERTY_FILE with the ZetaScale configuration file path
    Set the enviornmental variable: ZS_LIB with the Zetascale dynamic library filepath
    
    
    TESTS:
    (1) format the storage with this utlity and check an FDF application can come up successfully without REFORMAT=1
    (2) Check if the utlity can format the storage without REFORMAT=1 specified in the configuration 
    (3) Check if the utlity returns error if wrong storage is specified
    (4) check if the ulity returns error if required enviornmental variables are not set 
    
    
    
    
    
    
    
    
    git-svn-id: https://10.196.60.217/svn/schooner-products/branches/fdf-3.0@2462 01a69087-22d5-4a29-8152-8a1d5e10e5e9

---
+++ zsck/zsformat.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <getopt.h>
+#include <zs.h>
+
+static struct ZS_state* zs_state;
+
+static struct option long_options[] = { 
+    {"help",    no_argument,       0, 'h'}, 
+    {0, 0, 0, 0} 
+};
+
+void print_help(char *pname) 
+{
+    fprintf(stderr, "Formats the ZetaScale Storage\n");
+    fprintf(stderr, "Set the enviornmental variable: ZS_PROPERTY_FILE with the ZetaScale configuration file path\n");
+    fprintf(stderr, "Set the enviornmental variable: ZS_LIB with the Zetascale dynamic library filepath\n");
+    
+}
+
+int get_options(int argc, char *argv[])
+{
+    int option_index = 0;
+    int c;
+
+    while (1) { 
+        c = getopt_long (argc, argv, "h", long_options, &option_index); 
+        if (c == -1) 
+            break;
+     
+        switch (c) { 
+        case 'h': 
+            print_help(argv[0]); 
+            return -1;
+        default:
+            print_help(argv[0]); 
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int init_zs() {
+    ZS_status_t status = ZS_FAILURE;
+    if ( ZS_SUCCESS != ( status = ZSInit( &zs_state ) ) ) {
+        fprintf(stderr, "Failed to initialize ZetaScale\n");
+        return status;
+    }
+    return status;
+}
+
+int close_zs()
+{
+    ZS_status_t status = ZS_FAILURE;
+    if ( ZS_SUCCESS != (status = ZSShutdown( zs_state ) ) ) {
+        fprintf(stderr, "Failed to shutdown ZetaScale!\n");
+        return status;
+    }
+    return status;
+}
+
+int main(int argc, char *argv[])
+{
+    if ( get_options( argc, argv) ) {
+        return -1;
+    }
+
+    if( getenv("ZS_PROPERTY_FILE") == NULL ) {
+        fprintf(stderr,"Formating failed:The enviornmental variable ZS_PROPERTY_FILE is not set\n");
+        exit(1);
+    }
+    if( getenv("ZS_LIB") == NULL ) {
+        fprintf(stderr,"Formating failed:The enviornmental variable ZS_LIB is not set\n");
+        exit(1);
+    }
+    printf("Formating the ZetaScale storage based on the configuration:%s\n", getenv("ZS_PROPERTY_FILE"));
+    printf("**********************************************\n");
+    ZSSetProperty("ZS_REFORMAT", "1");
+    if( init_zs() != ZS_SUCCESS ) {
+        printf("**************************\n");
+        fprintf(stderr,"\n\nFormating failed: Intilization of ZetaScale failed. Please check the configuration\n");
+    }
+    close_zs();
+    printf("****************\n");
+    printf("Formating completed\n");
+    return 0;
+}
